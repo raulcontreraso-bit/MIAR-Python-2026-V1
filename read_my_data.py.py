c682c93c39aa686e93b22c2a953a4610cf6b087d
raulcontreraso-bit <raulcontreraso@gmail.com>
2026-04-28 21:58:12 +0200

    k
    l

---
+++ read_my_data.py.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+# We use the read_excel function to bring the file into Python
+df = pd.read_excel('grades.xlsx')
+
+# This shows us the first few rows to make sure it worked
+print("--- Data Loaded Successfully ---")
+print(df.head())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
